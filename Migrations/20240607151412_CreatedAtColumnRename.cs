8dd2873d081d3a2e9998d3d16127bb05931e1434
akashbakshi-ubuntu-gamingpc <akash1996@hotmail.com>
2024-06-21 09:14:13 -0400

    - Added migration files and docker compose file

---
+++ Migrations/20240607151412_CreatedAtColumnRename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAtColumnRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Tweets",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Tweets",
+                newName: "CreateAt");
+        }
+    }
+}
---
+++ Migrations/20240607155326_TweetArchiveColumnAdd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TweetArchiveColumnAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Tweets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Tweets");
+        }
+    }
+}
